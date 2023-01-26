7f332b1e32daf780e47061a53f72477e3259df38
elishababu123 <119730168+elishababu123@users.noreply.github.com>
2023-01-26 19:19:41 +0530

    // Online C compilerto run C program online #include<stdio.h> int main() {     int a;     float x,p;     scanf("%d",&a);     x=3.14*a*a;     p=2*3.14*a;     printf("%0.2f %0.2f ",x,p); }

---
+++ Area_and_Perimeter_of_Circle.c
@@ -0,0 +1,13 @@
+// Online C compilerto run C program online
+#include<stdio.h>
+int main()
+{
+    int a;
+    float x,p;
+    scanf("%d",&a);
+    x=3.14*a*a;
+    p=2*3.14*a;
+    printf("%0.2f
+%0.2f
+",x,p);
+}
