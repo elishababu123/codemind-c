be9b28b4491aeb0115109b817d5e471ecf456512
elishababu123 <119730168+elishababu123@users.noreply.github.com>
2023-01-26 23:20:14 +0530

    #include<stdio.h> int main() {     int n,c=0,rem,ec=0,oc=0;     scanf("%d",&n);     while(n>0)     {         rem=n%10;         c=c+1;         if(rem%2==0)         {             ec=ec+1;
            }
            else if(rem%2!=0)
            {
                oc=oc+1;
                
            }
            n=n/10;
            
        }
        if(c==ec)
        {
            printf("Even");
            
        }
        else if(c==oc)
        {
            printf("Odd");
            
        }
        else
        {
            printf("Mixed");
            
        }
        
    }

--- Even_Odd_Mixed.c
+++ Even_Odd_Mixed.c
@@ -5,30 +5,35 @@ int main()
     scanf("%d",&n);
     while(n>0)
     {
-        rem=n%10;c=c+1;
+        rem=n%10;
+        c=c+1;
         if(rem%2==0)
         {
             ec=ec+1;
+            
         }
-        else
-        if(rem%2!=0)
+        else if(rem%2!=0)
         {
             oc=oc+1;
+            
         }
         n=n/10;
+        
     }
     if(c==ec)
     {
         printf("Even");
+        
     }
-    else 
-    if(c==oc)
+    else if(c==oc)
     {
         printf("Odd");
+        
     }
     else
     {
         printf("Mixed");
+        
     }
     
 }
