43a8f39cba07066dbad990969d2ae7a7e1d684d7
elishababu123 <119730168+elishababu123@users.noreply.github.com>
2023-01-26 18:48:50 +0530

    #include<stdio.h> int main() {     float bs,hra,da,pf,gs;     scanf("%f%f%f",&bs,&hra,&da);     pf=0.12*bs;gs=bs+hra+da+pf;     printf("%.2f %.2f ",pf,gs); }

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -3,8 +3,7 @@ int main()
 {
     float bs,hra,da,pf,gs;
     scanf("%f%f%f",&bs,&hra,&da);
-    pf=0.12*bs;
-    gs=bs+hra+da+pf;
+    pf=0.12*bs;gs=bs+hra+da+pf;
     printf("%.2f
 %.2f
 ",pf,gs);
