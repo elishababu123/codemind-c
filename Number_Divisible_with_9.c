49d33259a0d91f3dd0f0dcaebbea7c11b34fcaa6
elishababu123 <119730168+elishababu123@users.noreply.github.com>
2023-01-26 22:56:16 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%9==0)     {         printf("True");
        }
        else
        {
            printf("False");
            
        }
        
    }

---
+++ Number_Divisible_with_9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%9==0)
+    {
+        printf("True");
+        
+    }
+    else
+    {
+        printf("False");
+        
+    }
+    
+}
